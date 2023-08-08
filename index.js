111ea0628e1004f687c220dcc3a55ddbc6c2c105
Gituser <pvmohan232@gmail.com>
2023-08-08 11:23:33 +0530

    Update index.js

--- index.js
+++ index.js
@@ -77,6 +77,14 @@ function addlistitem() {
             myfunction(e.target);
        
         });
+
+        item.addEventListener('keydown' ,(e)=>{
+            if(e.keyCode==13) {
+                e.preventDefault();
+                 return false; 
+            }
+            
+        });
         
     }) ;
 
